0b5fa6c1d42108847fe6c1157040e152b2e0e97c
Vincent Demeester <vincent@sbr.pm>
2023-10-19 16:10:07 +0200

    Prepare release 0.4.0
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- release.sh
+++ release.sh
@@ -20,3 +20,6 @@ git commit -sS -m "Prepare release $VERSION" || true
 mkdir -p release
 ${CATALOGCD} release --output release  --version="${VERSION}" ${TASKS}
 
+git tag v${VERSION}
+git push origin v${VERSION}
+gh release create --generate-notes -t "v${VERSION}" v${VERSION} release/catalog.yaml release/resources.tar.gz
